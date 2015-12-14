495973e840d46741ae703968024ab119944fcddc
Adriano Ferreira <adriano.ferreira@me.com>
2015-12-14 17:57:26 -0500

    Split extensions files for organization purpose

--- Sort.playground/Sources/SequenceType.swift
+++ Sort.playground/Sources/SequenceType.swift
@@ -37,38 +37,3 @@ public extension SequenceType where Generator.Element: Comparable {
         return isSorted(<=)
     }
 }
-
-public extension CollectionType {
-    
-    /// Shuffles the elements of `self`
-    ///
-    /// - returns: A copy of `self` with its elements shuffled
-    
-    func shuffle() -> [Generator.Element] {
-        var array = Array(self)
-        array.shuffleInPlace()
-        return array
-    }
-}
-
-public extension MutableCollectionType where Index == Int {
-    
-    /// Shuffles the elements of `self` in-place using Fisher-Yeates algorithm
-    
-    mutating func shuffleInPlace() {
-        
-        guard count > 1 else {
-            return
-        }
-        
-        for i in 0..<count - 1 {
-            let j = i + Int(arc4random_uniform(UInt32(count - i)))
-            
-            guard i != j else {
-                continue
-            }
-            
-            swap(&self[i], &self[j])
-        }
-    }
-}
