640a4724977d5d215286b4716a14556883b958a1
Adriano Ferreira <adriano.ferreira@me.com>
2015-12-19 22:44:25 -0500

    Fix typos

--- Sort.playground/Pages/Table of Contents.xcplaygroundpage/Contents.swift
+++ Sort.playground/Pages/Table of Contents.xcplaygroundpage/Contents.swift
@@ -2,13 +2,13 @@
 //:
 //: > **Important:**
 //: > This is a preliminary document of sorting algorithms writen in the [Swift Programming Language](https://developer.apple.com/swift/).
-//: > The code was tested on _Xcode 7.2_ and _Swift 2.1_.
+//: > The code was tested in _Xcode 7.2_ and _Swift 2.1_.
 //:
 //: Welcome to the Sort Playground!
 //:
 //: The idea here is to have fun while exploring alternative implementations of different sorting techniques written in Swift, a modern and very exciting programming language. So, to better experience how to move from disarray to order when managing collections of data, some variations of these sorting algorithms are provided, starting with a classic implementation that is then forged over and over until it becomes more idiomatic, or simply, more Swift-y.
 //:
-//: Remember, the focus of this playground is experimentation, learning, and having fun. Do not expect any crazy dilly-dily award-winning performant code ahead, ok?
+//: Remember, the focus of this playground is experimentation, learning, and having fun. Do not expect any crazy dilly-dilly award-winning performant code ahead, ok?
 //:
 //: Now, you're about to indulge yourself with the following takes:
 //:
@@ -26,7 +26,7 @@
 //:
 //: I know, I know... Talk is cheap, show me the code, right?
 //:
-//: Okey-dokey, so let's dive in and enjoy the ride!
+//: Okey-dokey, let's dive in and enjoy the ride!
 //:
 //: ### Table of Contents
 //:
@@ -38,6 +38,6 @@
 //:
 //: Check [What's Next](What's%Next) for more details on the evolution of this playground.
 //:
-//: > Hey, feel free to check [this repo](https://github.com/adrfer/Sort) at GitHub, and/or [ping me](https://twitter.com/_adrfer) on Twitter.
+//: > Hey, feel free to check [this playground's repository](https://github.com/adrfer/Sort) at GitHub, and/or [ping me](https://twitter.com/_adrfer) on Twitter.
 //:
 //: [Next](@next)
