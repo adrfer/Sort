18d73ff625c08812ede3a277e51d2cbe772e0618
Adriano Ferreira <adriano.ferreira@me.com>
2015-12-22 13:28:27 -0500

    Update link to `sort` from the Swift Standard Library at the What's Next page

--- Sort.playground/Pages/What's Next.xcplaygroundpage/Contents.swift
+++ Sort.playground/Pages/What's Next.xcplaygroundpage/Contents.swift
@@ -6,7 +6,7 @@
 //:
 //: Here are some future ideas yet to be considered:
 //:
-//: - Review and/or include a version that takes a closure/predicate, similar to `sort` from the [Swift Standard Library](https://developer.apple.com/library/ios//documentation/Swift/Reference/Swift_MutableCollectionType_Protocol/index.html#//apple_ref/swift/intfm/MutableCollectionType/s:FeRq_Ss21MutableCollectionTypeqq_Ss16MutableIndexable5IndexSs21RandomAccessIndexType_SsS_9partitionuRq_S_qq_S0_5IndexS1__FRq_FTGVSs5Rangeqq_S0_5Index_15isOrderedBeforeFTqqq_Ss14CollectionType9GeneratorSs13GeneratorType7Elementqqq_S3_9GeneratorS4_7Element_Sb_qq_S0_5Index)
+//: - Review and/or include a version that takes a closure/predicate, similar to `sort` from the [Swift Standard Library](https://developer.apple.com/library/ios//documentation/Swift/Reference/Swift_MutableCollectionType_Protocol/index.html#//apple_ref/swift/intfm/MutableCollectionType/s:FeRq_Ss21MutableCollectionType_SsS_4sortuRq_S__Fq_FFTqqq_Ss14CollectionType9GeneratorSs13GeneratorType7Elementqqq_S0_9GeneratorS1_7Element_SbGSaqqq_S0_9GeneratorS1_7Element_)
 //: - Refactor tests into its own playground page, if possible
 //: - Implement Heap Sort and Shell Sort algorithms
 //:
