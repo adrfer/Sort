af54e093297cc4c1ea7bcd95770e9978f04a634b
Adriano Ferreira <adriano.ferreira@me.com>
2016-01-12 16:06:31 -0500

    Update What's Next section

--- Sort.playground/Pages/What's Next.xcplaygroundpage/Contents.swift
+++ Sort.playground/Pages/What's Next.xcplaygroundpage/Contents.swift
@@ -6,7 +6,6 @@
 //:
 //: Here are some future ideas yet to be considered:
 //:
-//: - Review and/or include a version that takes a closure/predicate, similar to `sort` from the [Swift Standard Library](https://developer.apple.com/library/ios//documentation/Swift/Reference/Swift_MutableCollectionType_Protocol/index.html#//apple_ref/swift/intfm/MutableCollectionType/s:FeRq_Ss21MutableCollectionType_SsS_4sortuRq_S__Fq_FFTqqq_Ss14CollectionType9GeneratorSs13GeneratorType7Elementqqq_S0_9GeneratorS1_7Element_SbGSaqqq_S0_9GeneratorS1_7Element_)
 //: - Refactor tests into its own playground page, if possible
 //: - Implement Heap Sort and Shell Sort algorithms
 //:
