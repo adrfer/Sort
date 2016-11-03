7bc7d269a25c0e3eaad77c0e5c1c9538bd9271bc
Adriano Ferreira <adriano.ferreira@me.com>
2016-11-03 09:58:01 -0400

    Improve table of contents page

--- Sort.playground/Pages/Table of Contents.xcplaygroundpage/Contents.swift
+++ Sort.playground/Pages/Table of Contents.xcplaygroundpage/Contents.swift
@@ -2,20 +2,20 @@
 //:
 //: Welcome to the Sort Playground!
 //:
-//: The idea here is to have fun while exploring alternative implementations of different sorting techniques written in Swift, a modern and very exciting programming language. So, to better experience how to move from disarray to order when managing collections of data, some variations of these sorting algorithms are provided, starting with a classic implementation that is then forged over and over until it becomes more idiomatic, or simply, more Swift-y.
+//: The idea here is to have fun while exploring alternative implementations of different sorting techniques written in [Swift](http://swift.org), a modern and very exciting programming language. So, to better experience how to move from disarray to order when managing collections of data, some variations of these sorting algorithms are provided, starting with a classic implementation that is then forged over and over until it becomes more idiomatic, or simply, more Swift-y.
 //:
-//: Remember, the focus of this playground is experimentation, learning, and having fun. Do not expect any crazy dilly-dilly award-winning performant code ahead, ok?
+//: > Remember, the focus of this playground is experimentation, learning, and having fun. Do not expect any crazy award-winning performant code ahead, ok?
 //:
 //: Now, you're about to indulge yourself with the following takes:
 //:
-//: - The Classic, a die-hard style, rooted in tradition, in all its imperative glory
-//: - The Swift-ish, a sligthly more modern take on the classic, but still not quite quaint enough
-//: - The Swiftest, a nifty approach that attempts to tap into the most powerful language features yet
-//: - The Generic, a play on the swiftest version, but elevated to a type-agnostic nirvana status
-//: - The Functional, a quirky take that unleashes some of the neat declarative aspects of the language
-//: - The Bonus, a twist on the Swift-ish version that takes a predicate, similar to `sort` from the [Swift Standard Library](https://developer.apple.com/library/ios//documentation/Swift/Reference/Swift_MutableCollectionType_Protocol/index.html#//apple_ref/swift/intfm/MutableCollectionType/s:FesRxs21MutableCollectionTypeWx9Generator7Element_s10ComparablerS_4sortFT_GSaWxS0_S1___)
+//: - The Classic: a die-hard style, rooted in tradition, in all its imperative glory
+//: - The Swift-ish: a sligthly more modern take on the classic, but still not quite quaint enough
+//: - The Swiftest: a nifty approach that attempts to tap into the most powerful features of the language yet
+//: - The Generic: a play on the swiftest version, but elevated to a type-agnostic nirvana status
+//: - The Functional: a quirky take that unleashes some of the neat declarative aspects of the language
+//: - The Bonus: a twist on the swift-ish version that takes a predicate, similar to `sort` from the [Swift Standard Library](https://developer.apple.com/reference/swift)
 //:
-//: Also, a few test cases covering some initial conditions were added:
+//: Also, a few test cases covering some initial conditions are included:
 //:
 //: - Sorted
 //: - Nearly Sorted
@@ -24,7 +24,7 @@
 //:
 //: I know, I know... Talk is cheap, show me the code, right?
 //:
-//: Okey-dokey, let's dive in and enjoy the ride!
+//: Ok then, let's dive in and enjoy the ride!
 //:
 //: ### Table of Contents
 //:
