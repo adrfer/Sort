68b3a0f11ead05e4ebe3f0983e8df9f31a38118a
Adriano Ferreira <adriano.ferreira@me.com>
2016-11-06 20:28:25 -0500

    Rename parameters of function random on Integer extension

--- Sort.playground/Sources/Integer.swift
+++ Sort.playground/Sources/Integer.swift
@@ -8,13 +8,13 @@ public extension Integer {
     ///
     /// - Parameters:
     ///
-    ///   - lower: Inclusive lower bound random value
-    ///   - upper: Inclusive upper bound random value
+    ///   - lowerBound: Inclusive lower bound value
+    ///   - upperBound: Inclusive upper bound value
     ///
-    /// - Returns: A random integer between `lower` and `upper` bounds
+    /// - Returns: A random integer between `lowerBound` and `upperBound` values
 
-    static func random(from lower: Int, to upper: Int) -> Int {
+    static func random(from lowerBound: Int, to upperBound: Int) -> Int {
         
-        return lower + Int(arc4random_uniform(UInt32(upper - lower + 1)))
+        return lowerBound + Int(arc4random_uniform(UInt32(upperBound - lowerBound + 1)))
     }
 }
